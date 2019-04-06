cb40b92e5c7d7c3f8001866fa212c0d326ad93bb
Matt F <matomatical@gmail.com>
2019-04-06 18:53:38 +1100

    WIP everything-other-than-expr parsers

--- code/stage1/src/GoatParser.hs
+++ code/stage1/src/GoatParser.hs
@@ -7,6 +7,9 @@ import Text.Parsec.Expr
 import Text.Parsec.Language
 import qualified Text.Parsec.Token as Token
 
+type Parser a
+   = Parsec String Int a
+
 -- ----------------------------------------------------------------------------
 -- Token parser generation
 
@@ -65,7 +68,113 @@ stringLiteral = Token.stringLiteral lexer
 -- ----------------------------------------------------------------------------
 -- Program parsing
 
+parseGoatProgram :: Parser GoatProgram
+parseGoatProgram
+  = do
+      whiteSpace
+      procs <- many1 pProc
+      eof
+      return (GoatProgram procs)
 
+pProc :: Parser Proc
+pProc
+  = do
+      reserved "proc"
+      name <- identifier
+      params <- parens (commaSep pParam)
+      decls <- many pDecl
+      reserved "begin"
+      stmts <- many1 pStmt
+      reserved "end"
+      return (Proc name params decls stmts)
+
+pParam :: Parser Param
+pParam
+  = do
+      passBy <- pPassBy
+      baseType <- pBaseType
+      name <- identifier
+      return (Param passBy baseType name)
+
+pPassBy :: Parser PassBy
+pPassBy
+  =   reserved "val" >> return Val
+  <|> reserved "ref" >> return Ref
+
+pBaseType :: Parser BaseType
+pBaseType
+  =   reserved "bool"  >> return BoolType
+  <|> reserved "float" >> return FloatType
+  <|> reserved "int"   >> return IntType
+
+pDecl :: Parser Decl
+pDecl
+  = do
+      baseType <- pBaseType
+      name <- identifier
+      dim <- pDim
+      return (Decl baseType name dim)
+
+-- this is a trickier one...?
+pDim :: Parser Dim
+pDim
+  = do
+      -- TODO:
+      -- return Dim0 or Dim1 n or Dim2 n m
+      return Dim0
+
+pStmt :: Parser Stmt
+pAgn, pRead, pWrite, pCall, pIf, pWhile :: Parser Stmt
+pStmt
+  = choice [pAsg, pRead, pWrite, pCall, pIf, pWhile]
+
+pAsg
+  = do
+      var <- pVar
+      reservedOp ":="
+      expr <- pExpr
+      semi
+      return (Asg var expr)
+pRead
+  = do
+      reserved "read"
+      var <- pVar
+      semi
+      return (Read var)
+pWrite
+  = do
+      reserved "write"
+      expr <- pExpr
+      semi
+      return (Write expr)
+pCall
+  = do
+      reserved "call"
+      name <- identifier
+      args <- parens (commaSep pExpr)
+      semi
+      return (Call name args)
+pIf
+  = do
+      -- TODO:
+      return ()
+pWhile
+  = do
+      reserved "while"
+      expr <- pExpr
+      reserved "do"
+      stmts <- many1 stmt -- TODO: check many1 right!?
+      reserved "od"
+      return (While expr stmt)
+
+pVar :: Parser Var
+pVar
+  = do
+    -- TODO:
+    return ()
+
+
+-- assumes a function named `pExpr :: Parser Expr` is defined below
 
 -- ----------------------------------------------------------------------------
 -- Expression Parsing
