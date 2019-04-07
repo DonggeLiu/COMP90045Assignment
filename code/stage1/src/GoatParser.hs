cdf0ec44071d52145584bdda163bc2339bae498c
Matt F <matomatical@gmail.com>
2019-04-07 22:21:40 +1000

    cleaned up and documented suffixMaybe and manyMN etc.

--- code/stage1/src/GoatParser.hs
+++ code/stage1/src/GoatParser.hs
@@ -132,9 +132,23 @@ pDecl
   = do
       baseType <- pBaseType
       name <- identifier
-      dim <- pDim -- see the parser pDim much later in this file
+      dim <- pDim
       semi
       return (Decl baseType name dim)
+
+-- DIM        -> ε | "[" int  "]" | "[" int  "," int  "]"
+pDim :: Parser Dim
+pDim
+  = do
+      -- see 'suffixMaybe' combinator definition and motivation, far below
+      -- TODO: FIX THIS UGLY HACK --. we need our version of integer to return
+      -- Int directly?             ,'--------------------------------.
+      size <- suffixMaybe (integer >>= (\i -> return (fromIntegral i)))
+      case size of
+        Nothing    -> return (Dim0)
+        Just [n]   -> return (Dim1 n)
+        Just [n,m] -> return (Dim2 n m)
+
 
 
 -- STMT       -> ASGN | READ | WRITE | CALL | IF | WHILE 
@@ -205,9 +219,24 @@ pWhile
       return (While cond stmts)
 
 
+-- VAR        -> id SUBSCRIPT
+-- SUBSCRIPT  -> ε | "[" EXPR "]" | "[" EXPR "," EXPR "]"
+pVar :: Parser Var
+pVar
+  = do
+      name <- identifier
+      -- see 'suffixMaybe' combinator definition and motivation, below
+      subscript <- suffixMaybe pExpr
+      case subscript of
+        Nothing    -> return (Var0 name)
+        Just [i]   -> return (Var1 name i)
+        Just [i,j] -> return (Var2 name i j)
 
--- Now, for capturing the similarity that exists between the VAR and DIM rules:
--- 
+
+
+-- Now, for capturing the similarity that exists between the SUBSCRIPT and DIM 
+-- rules!
+--
 -- The Grammar rules:
 -- 
 --   (1) DIM        -> ε | "[" int  "]" | "[" int  "," int  "]"
@@ -215,53 +244,36 @@ pWhile
 --
 -- obviously have very similar structure. They are both of the form:
 --
---   Z_a            -> ε | "[" a "]" | "[" a "," a "]"
+--   S_a            -> ε | "[" a "]" | "[" a "," a "]"
 --
 -- where a is either an int or an expression.
 --
 -- It's be nice to capture this similarity in some kind of parser combinator
 -- (parametrised by a parser for a) to avoid repeating code!
---
--- Note that (1) represents possibly resizing a variable according to some
--- integer dimensions (simensionality, or shape), while (2) represents looking 
--- within a possibly large structure for a partciular element (indexing or 
--- subscripting):
---
---     X[12,7]---------------->                 .----------------------.
---     |                      |                 |     |                |
---     |                      |                 |     v                |
---     |                      |                 |---->X[3,2]           |
---     |                      |                 |                      |
---     |                      |                 |                      |
---     |                      |                 |                      |
---     V______________________|                 |______________________|
---               (1)                                      (2)
---
--- In the first case, we are 'zooming out' the variable to become an array /
--- matrix of variables. In the second case, we are 'zooming in' to a particular 
--- element of such an array / matrix.
 -- 
--- So... we shall name this [,] construct our 'zoom'!..  ...!?
---                                                       any other suggestions?
--- 
---   ZOOM_a         -> ε | "[" a "]" | "[" a "," a "]"
--- 
--- Left-factoring these productions leads to the following:
--- 
---   ZOOM_a         -> ε | "[" a ZOOM1_a "]"
---   ZOOM1_a        -> "," a | ε
--- 
--- We propose the following parser combinator; the 'zoom parser':
+-- Well, we can make one! Here it is:
 
-
--- zoom
--- "To _zoom_ a parser, possibly look for brackets containing one occurrence of 
--- the thing, followed by a possible second occurrence."
--- Return either Nothing or Just [x] or Just [x,y] with the 0, 1 or 2 results.
-zoom :: (Parser a) -> Parser (Maybe [a])
-zoom parser
+-- suffixMaybe
+-- (optionally) parse a suffix using provided parser and return either
+-- * Nothing (if no suffix present), or
+-- * Just [x] or Just [x,y] with the 1 or 2 results (if present)
+suffixMaybe :: (Parser a) -> Parser (Maybe [a])
+suffixMaybe parser
   = optionMaybe $ brackets $ commaSepMN 1 2 parser
 
+-- This parser uses commaSepMN, a general parser combinator in the spirit of
+-- parsec's commaSep and commaSep1. It's defined using the following general 
+-- combinators:
+
+-- manyMN
+-- Inspired by parsec's many and many1 combinators which gives us a way to
+-- express Kleene * and +, this parser combinator gives us a way to express
+-- minimum/maximum repetitions (that is, the {m, n} construct from regular
+-- expressions).
+-- Parse at least m and at most n occurrences of whatever p is looking for;
+-- returning the result as a list.
+-- Note: There may be more instances of p after this parser completes if it
+-- found exactly n, these will be left to the next parser to parse (or reject).
 manyMN :: Int -> Int -> (Parser a) -> (Parser [a])
 manyMN m n p
   | m<0 || n<m = error "manyMN expects 0 <= m <= n"
@@ -269,56 +281,37 @@ manyMN m n p
   | m == 0     = option [] $ p <:> manyMN 0 (n-1) p
   | otherwise  = p <:> manyMN (m-1) (n-1) p
 
+-- sepByMN
+-- Insipired by parsec's sepBy1 and sepBy, this combinator allows us to 
+-- look for a list of results from parser `p`, parsing a `sep` in between
+-- each one.
+-- It mainly relies on `manyMN (m-1) (n-1) (sep >> p)' but we must take 
+-- some care to treat the first instance correctly (it may be not allowed, 
+-- optional, or mandatory depending on m and n) and set off the right call
+-- to manyMN.
 sepByMN :: (Parser a) -> Int -> Int -> (Parser b) -> (Parser [b])
-sepByMN s m n p
+sepByMN sep m n p
   | m<0 || n<m = error "sepByMN expects 0 <= m <= n"
   | n == 0     = return []
-  | m == 0     = option [] $ p <:> manyMN 0 (n-1) (s >> p)
-  | otherwise  = p <:> manyMN (m-1) (n-1) (s >> p)
+  | m == 0     = option [] $ p <:> manyMN 0 (n-1) (sep >> p)
+  | otherwise  = p <:> manyMN (m-1) (n-1) (sep >> p)
 
+-- commaSepMN
+-- Inspired by parsec's commaSep parser combinator and reflecting common
+-- usage of sepByMN, we provide this alias function to parse a comma-separated
+-- list of some parser, between some minimum and maximum length.
 commaSepMN :: Int -> Int -> (Parser a) -> Parser [a]
 commaSepMN
   = sepByMN comma
 
--- (<:>) 'applicative cons' operator
--- mnemonic shortcut for using cons as an applicative function
--- (<:>) x xs
-  -- = liftA2 (:)
--- it's like:
-(<:>) a_x a_xs = (:) <$> a_x <*> a_xs
+-- (<:>)
+-- 'applicative cons' operator - A mnemonic shortcut for using cons as an 
+-- applicative function
+(<:>)
+  = liftA2 (:) -- liftA2: it's just like: (<:>) a_x a_xs = (:) <$> a_x <*> a_xs
 
 
 
--- DIM        -> ε | "[" int  "]" | "[" int  "," int  "]"
-pDim :: Parser Dim
-pDim
-  = do
-      size <- zoom (integer >>= (\i -> return (fromIntegral i)))
-      case size of
-        Nothing    -> return (Dim0)
-        Just [n]   -> return (Dim1 n)
-        Just [n,m] -> return (Dim2 n m)
-
--- VAR        -> id SUBSCRIPT
--- SUBSCRIPT  -> ε | "[" EXPR "]" | "[" EXPR "," EXPR "]"
-pVar :: Parser Var
-pVar
-  = do
-      name <- identifier
-      subscript <- zoom pExpr
-      case subscript of
-        Nothing    -> return (Var0 name)
-        Just [i]   -> return (Var1 name i)
-        Just [i,j] -> return (Var2 name i j)
-
--- 
--- Notes:
--- * Using a list to represent 0, 1, or 2 return values is not so great. It'd 
---   perhaps be cleaner to use a new type for this purpose?
---   `data Zoom a = Zoom0 | Zoom1 a | Zoom2 a a` ? The definitions of Var and 
---   Dim could be refactored to have an accompanying `Zoom Expr` or `Zoom Int` 
---   too.
--- 
 
 
 -- The above assumes a function named `pExpr :: Parser Expr` is defined below.
