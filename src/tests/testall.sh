f8ae305c249367a8573fa16cd3ac909a1f935e42
Matt F <matomatical@gmail.com>
2019-05-08 13:29:47 +1000

    improve integration test script output

--- src/tests/testall.sh
+++ src/tests/testall.sh
@@ -1,17 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-# 1. hook up your Goat executable (e.g. "./Goat") and sample dir:
+# START HERE:
+# hook up your Goat executable (e.g. "./Goat") and sample dir below
+
 GOAT="./Goat"
 SAMPLE_ROOT="Tests/samples"
 
-# Run GOAT to prettify all the goat programs (*.gt) under SAMPLE_ROOT,
-# and compare your pretty output to the existing pretty output (*.gt.pp)
+# Script constants and settings:
+
 FAIL='\033[0;31mFAIL\033[0m'
 WARN='\033[0;35mWARN\033[0m'
 shopt -s globstar
 
-# Testing good goat programs: compare output in *.gt to *.gt.pp
-echo "TEST: parser can correctly pretty-print all well-formed goat programs..."
+# RUN THE TESTS!
+
+# Run GOAT to prettify all the goat programs (*.gt) under SAMPLE_ROOT,
+# and compare your pretty output to the existing pretty output (*.gt.pp)
+echo "TEST: parser should correctly print well-formed goat programs..."
 for testin in "$SAMPLE_ROOT"/**/*.gt; do
     # running goat:
     "$GOAT" -p "$testin" > ".temp.gt" 2>&1
@@ -33,28 +38,30 @@ for testin in "$SAMPLE_ROOT"/**/*.gt; do
         cat ".temp.gt" | sed 's/^/  /'
     fi
 done
+printf "DONE! num tests:"; ls -1 "$SAMPLE_ROOT"/**/*.gt | wc -l
 
-echo "TEST: parser should reject all ill-formed goat programs..."
+# Run GOAT to parse all the baad goat programs (*.gt.bad) under SAMPLE_ROOT,
+# and ensure your parser rejects them (and sends the right error code)
+echo "TEST: parser should reject any ill-formed goat programs..."
 for testin in "$SAMPLE_ROOT"/**/*.gt.bad; do
     # running goat:
     "$GOAT" -p "$testin" > ".temp.gt" 2>&1
     
     if [ $? == 0 ]; then
         # if it succeeded, we have a problem! There should have been an error
-        echo -e "${FAIL}: $testin parse succeeds, but should have errored"
+        echo -e "${FAIL}: parsing $testin succeeded, but should have rejected:"
         if [ -f "$testin.out" ]; then
-            echo "here's the intended error and message:"
             cat "$testin.out" | sed 's/^/  /'
         else
-            echo "(NOTE: $testin.out missing---no example error message)"
+            echo "  ($testin.out missing---no example error message)"
         fi
     fi
     # if it failed, that's good! the error message is not specified, though, so
     # we need not compare it.
 done
-
+printf "DONE! num tests:"; ls -1 "$SAMPLE_ROOT"/**/*.gt.bad | wc -l
 
 # clean up temporary files:
 rm ".temp.gt"
 
-echo "DONE: see output above (no output means all tests passed)."
+echo "ALL DONE! (note: no output means all tests passed)."
