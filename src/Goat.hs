2ba58fa0596e9ce318295a0c194e9a5a30a9dbd2
Matt F <matomatical@gmail.com>
2019-05-09 20:43:53 +1000

    add feature: show source code context on syntax error.

--- src/Goat.hs
+++ src/Goat.hs
@@ -20,6 +20,9 @@ import System.Environment
 import Control.Monad (when, unless)
 import Data.List (nub, intersperse, intercalate, (\\))
 
+import Text.Parsec
+import Text.Parsec.Error
+
 import GoatLang.Parser (parseProgram)
 import GoatLang.PrettyPrint (prettify)
 
@@ -39,9 +42,7 @@ main
       sourceCode <- readFile sourceFileName
       let parsed = parseProgram sourceFileName sourceCode
       ast <- case parsed of
-        Left err  -> do
-            putStrLn $ "Parse error at " ++ show err
-            exitWith (ExitFailure 2)
+        Left err  -> syntaxErrorExit sourceCode err
         Right ast -> return ast
 
       -- handle the parsed program:
@@ -55,6 +56,40 @@ main
       -- compile AST into machine code, and output executable, if possible
       when (null flags || flagIsSet 'x' flags) $ do
           putStrLn "Sorry, can't generate code yet!"
+
+-- ----------------------------------------------------------------------------
+-- Handling errors in the source program
+-- ----------------------------------------------------------------------------
+
+-- syntaxErrorExit
+-- Display syntax error diagnostic information to the user, including the
+-- relevant section of the source program, then exit (with failure).
+syntaxErrorExit :: String -> ParseError -> IO a
+syntaxErrorExit src err
+  = do
+      let pos = errorPos err
+      let lineNum = sourceLine pos
+      let colNum = sourceColumn pos
+
+      putStrLn $ "Syntax error at " ++ show pos ++ ":"
+      -- show 3 lines of context:
+      putStr   $ unlines $ take (min lineNum 3) $ drop (lineNum-3) $ lines src
+      -- point to the problem:
+      putStr   $ take (colNum-1) (repeat ' ') ++ "^"
+      putStrLn $ showErrorMessagesDefaults (errorMessages err)
+
+      exitWith (ExitFailure 2)
+
+-- showErrorMessagesDefaults
+-- wrap Parsec's showErrorMessages function, which requires a bunch of template
+-- strings be provided.
+showErrorMessagesDefaults :: [Message] -> String
+showErrorMessagesDefaults
+  = showErrorMessages
+      "or" "unknown parse error" "expecting" "unexpected" "end of input"
+
+-- TODO: Something similar for semantic errors
+
 
 -- ----------------------------------------------------------------------------
 -- Processing command-line options
@@ -89,12 +124,12 @@ checkArgs
           helpExit
       let invalidFlags = flags \\ validFlags
       unless (null invalidFlags) $ do
-          errorExit $ "invalid flag(s): " ++ (intersperse ',' invalidFlags)
+          argsErrorExit $ "invalid flag(s): " ++ (intersperse ',' invalidFlags)
       let arguments = getArguments args
       case arguments of
         [sourceFileName] -> return (Opts flags sourceFileName)
-        []               -> errorExit $ "missing required argument: file"
-        (_:excess)       -> errorExit $ "excess positional argument(s): "
+        []               -> argsErrorExit $ "missing required argument: file"
+        (_:excess)       -> argsErrorExit $ "excess positional argument(s): "
                                         ++ intercalate ", " excess
 
 -- getFlags, getArguments
@@ -116,10 +151,10 @@ helpExit
       printUsage
       exitSuccess
 
--- errorExit
+-- argsErrorExit
 -- Display an error message and then exit (with failure)
-errorExit :: String -> IO a
-errorExit problem
+argsErrorExit :: String -> IO a
+argsErrorExit problem
   = do
       putStrLn $ "Argument error:\n  " ++ problem
       printUsage
@@ -190,7 +225,3 @@ printGoatHead
 --              '''|____/   \__/\___|
 --                      \_.  / _/
 --             valkyrie    \/\/
-
-
-
-
