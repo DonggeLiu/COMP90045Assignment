0e126a903b85f122885bb3d4833a3bd5e5fa4df8
Matt F <matomatical@gmail.com>
2019-05-16 23:29:06 +1000

    fix milestone 1 binary and unary operations

--- src/GoatLang/CodeGen.hs
+++ src/GoatLang/CodeGen.hs
@@ -26,6 +26,9 @@ genCode (GoatProgram [main])
               , genCodeProc main
               ] 
 
+-- TODO:
+-- 3. Follow instructions in slack about starting milestone 2.
+
 genCodeProc :: Proc -> InstrTree
 genCodeProc (Proc (Id "main") [] [] stmts) -- TODO: allow declarations in main
   = InstrList [ InstrLabel $ ProcLabel "main"
@@ -61,39 +64,92 @@ lookupPrintBuiltin expr
       FloatType -> PrintReal
       IntType -> PrintInt
 
+
 genCodeExprInto :: Reg -> Expr -> InstrTree
 genCodeExprInto register (IntConst int)
   = InstrLeaf $ IntConstInstr register int
 genCodeExprInto register (FloatConst float)
   = InstrLeaf $ RealConstInstr register float
-genCodeExprInto register (BoolConst bool)
-  = case bool of
-      True -> InstrLeaf $ IntConstInstr register 1
-      False -> InstrLeaf $ IntConstInstr register 0
+genCodeExprInto register (BoolConst True)
+  = InstrLeaf $ IntConstInstr register 1
+genCodeExprInto register (BoolConst False)
+  = InstrLeaf $ IntConstInstr register 0
+
+-- Treat non-strict operations specially:
+
+-- 'And' will not actually use oz's 'and' instruction (which is strict).
+-- Instead, we'll:
+-- * Load the first operand into the target register.
+-- * If false (0), we are done! Skip evaluating the second operand, and leave
+--   the 0 in the target register.
+-- * If true (1), we need to check the second operand. Load into target
+--   register.
+-- * Whatever the result, leave it in the target register (that's the result of
+--   the And operation).
+genCodeExprInto register (BinExpr And l r)
+  = InstrList [ genCodeExprInto register l
+              , InstrLeaf $ BranchOnFalseInstr register afterLabel
+              , genCodeExprInto register r
+              , InstrLabel afterLabel
+              ]
+  where
+    afterLabel = BlockLabel 0 -- TODO: Get an actual new label
+
+-- Or is similar (but we skip the second operand when the first is True,
+-- rather than False).
+genCodeExprInto register (BinExpr Or l r)
+  = InstrList [ genCodeExprInto register l
+              , InstrLeaf $ BranchOnTrueInstr register afterLabel
+              , genCodeExprInto register r
+              , InstrLabel afterLabel
+              ]
+  where
+    afterLabel = BlockLabel 0 -- TODO: Get an actual new label
+
 genCodeExprInto register (BinExpr op l r)
-  = InstrList
-    [ genCodeExprInto register l
-    , genCodeExprInto (succ register) r
-    , generateBinOpInstrTree register register (succ register) op lType rType
-    ]
-    where
-      lType = getExprType l
-      rType = getExprType r
+  = InstrList [ genCodeExprInto register l
+              , genCodeExprInto (succ register) r
+              , generateBinOpInstrTree register register (succ register) op lType rType
+              ]
+  where
+    lType = getExprType l
+    rType = getExprType r
+
+genCodeExprInto register (UnExpr Not expr)
+  = InstrList [ genCodeExprInto register expr
+              , InstrLeaf $ NotInstr register register
+              ]
+
+genCodeExprInto register (UnExpr Neg expr)
+  = InstrList [ genCodeExprInto register expr
+              , InstrLeaf $ instruction register register
+              ]
+  where
+    instruction = case getExprType expr of
+      FloatType -> NegRealInstr
+      IntType -> NegIntInstr
 
 generateBinOpInstrTree :: Reg -> Reg -> Reg -> BinOp -> BaseType -> BaseType
   -> InstrTree
+
+generateBinOpInstrTree destReg lReg rReg op IntType IntType
+  = InstrLeaf $ (lookupOpInt op) destReg lReg rReg
+
+generateBinOpInstrTree destReg lReg rReg op BoolType BoolType
+  = InstrLeaf $ (lookupOpBool op) destReg lReg rReg
+
+generateBinOpInstrTree destReg lReg rReg op FloatType FloatType
+  = InstrLeaf $ (lookupOpReal op) destReg lReg rReg
+
+-- maybe we have an int and a real
+-- TODO: 
+-- Annotate the AST with additional 'float cast' nodes to avoid this case
+-- E.g.: `Add FloatType (FloatCast (IntConst 4)) (FloatConst 4.0)`.
 generateBinOpInstrTree destReg lReg rReg op lType rType
-  = case (lType, rType) of
-      (IntType, IntType) -> InstrLeaf intInstruction
-      (BoolType, BoolType) -> InstrLeaf boolInstruction
-      otherwise -> InstrList [ realify lReg lType
-                             , realify rReg rType
-                             , InstrLeaf floatInstruction
-                             ]
-      where
-        floatInstruction = (lookupOpReal op) destReg lReg rReg
-        intInstruction = (lookupOpInt op) destReg lReg rReg
-        boolInstruction = (lookupOpBool op) destReg lReg rReg
+  = InstrList [ realify lReg lType
+              , realify rReg rType
+              , InstrLeaf $ (lookupOpIntOrReal op) destReg lReg rReg
+              ]
 
 realify :: Reg -> BaseType -> InstrTree
 realify _ FloatType
@@ -123,6 +179,24 @@ lookupOpReal GTh
 lookupOpReal GEq
   = GEqRealInstr
 
+lookupOpIntOrReal :: BinOp -> (Reg -> Reg -> Reg -> Instruction)
+lookupOpIntOrReal Add
+  = AddRealInstr
+lookupOpIntOrReal Sub
+  = SubRealInstr
+lookupOpIntOrReal Mul
+  = MulRealInstr
+lookupOpIntOrReal Div
+  = DivRealInstr
+lookupOpIntOrReal LTh
+  = LThRealInstr
+lookupOpIntOrReal LEq
+  = LEqRealInstr
+lookupOpIntOrReal GTh
+  = GThRealInstr
+lookupOpIntOrReal GEq
+  = GEqRealInstr
+
 lookupOpInt :: BinOp -> (Reg -> Reg -> Reg -> Instruction)
 lookupOpInt Add
   = AddIntInstr
@@ -146,10 +220,6 @@ lookupOpInt GEq
   = GEqIntInstr
 
 lookupOpBool :: BinOp -> (Reg -> Reg -> Reg -> Instruction)
-lookupOpBool And
-  = AndInstr
-lookupOpBool Or
-  = OrInstr
 lookupOpBool Equ
   = EquIntInstr
 lookupOpBool NEq
