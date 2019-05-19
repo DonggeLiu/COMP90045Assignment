8ecfb4b5d6d1b64f58056d3df4de0a9d5f33e9d2
Matt F <matomatical@gmail.com>
2019-05-19 10:42:32 +1000

    add coloured output to miletone test script (not the version that runs on the emulator, though)

--- src/tests/milestones.sh
+++ src/tests/milestones.sh
@@ -9,9 +9,9 @@ MILES_DIR="tests/samples/stage3-miles/"
 
 for n in {1..6}; do
     echo "--- Compiling mile$n.gt ---"
-    ./Goat "$MILES_DIR/mile$n.gt" || break
+    ./Goat -xl "$MILES_DIR/mile$n.gt" || break
     echo "--- Emulating mile$n.oz ---"    
-    ./Goat "$MILES_DIR/mile$n.gt" > "mile$n.oz"
+    ./Goat -x "$MILES_DIR/mile$n.gt" > "mile$n.oz"
     $OZ -s "mile$n.oz" || echo "ERROR (did you make the emulator?)"
     # TODO: Diff output with 'expected' (where do we get expected output?)
 done
