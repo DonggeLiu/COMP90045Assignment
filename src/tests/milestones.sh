33665063bb9ee537d20002d76543f3b412d04e52
Matt F <matomatical@gmail.com>
2019-05-24 19:52:30 +1000

    tweak milestone test script

--- src/tests/milestones.sh
+++ src/tests/milestones.sh
@@ -8,11 +8,13 @@ OZ="../oz/oz"
 MILES_DIR="tests/samples/stage3-miles/"
 
 for n in {1..6}; do
+    echo "--- Printing mile$n.gt ---"
+    ./Goat -pl "$MILES_DIR/mile$n.gt" || break
     echo "--- Compiling mile$n.gt ---"
     ./Goat -xl "$MILES_DIR/mile$n.gt" || break
     echo "--- Emulating mile$n.oz ---"    
     ./Goat -x "$MILES_DIR/mile$n.gt" > "mile$n.oz"
-    $OZ -s "mile$n.oz" || echo "ERROR (did you make the emulator?)"
+    $OZ -s "mile$n.oz" || { echo "ERROR" ; break ; }
     # TODO: Diff output with 'expected' (where do we get expected output?)
 done
 echo "--- Removing tempfiles ---"
