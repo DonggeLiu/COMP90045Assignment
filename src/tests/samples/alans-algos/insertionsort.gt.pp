475c329e1cee89f9ebb5209a9185090b4e6a9195
Alan Ung <alanu@student.unimelb.edu.au>
2019-05-27 21:50:27 +1000

    Fix behaviour of samples: bubblesort, insertionsort, prime_sieve, selectionsort

--- src/tests/samples/alans-algos/insertionsort.gt.pp
+++ src/tests/samples/alans-algos/insertionsort.gt.pp
@@ -10,16 +10,26 @@ begin
         write i + 1;
         write ": ";
         read numbers[i];
+        i := i + 1;
     od
     i := 1;
     while i < 10 do
         j := i;
         while (j > 0) && (numbers[j - 1] > numbers[j]) do
-            call int_swap(numbers[j - i], numbers[j]);
+            call int_swap(numbers[j - 1], numbers[j]);
             j := j - 1;
         od
         i := i + 1;
     od
+    i := 0;
+    while i < 10 do
+        if i != 0 then
+            write ", ";
+        fi
+        write numbers[i];
+        i := i + 1;
+    od
+    write "\n";
 end
 
 proc int_swap (ref int a, ref int b)
