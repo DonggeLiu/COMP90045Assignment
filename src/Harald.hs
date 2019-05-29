f06d301ef079a9fede627cbb24f039cef094500e
Matt Farrugia <matomatical@users.noreply.github.com>
2019-05-29 22:07:11 +1000

    Harald say (#19)
    * oops, `(take n) . repeat` <=> `replicate n`
    
    * 256-colour version of rainbow-masking GoatSay (but no speech marker)
    
    * added rainbow harald, removed the Goat
    
    * Clean up Harald.hs and ColourParTTY.hs
    
    * add harald to automatic submission, with a new target `make demo` we can tell him about
    
    * Harald eats carriage returns for breakfast

---
+++ src/Harald.hs
@@ -0,0 +1,168 @@
+module Main where
+
+-- ----------------------------------------------------------------------------
+--    COMP90045 Programming Language Implementation, Assignment Stage 3
+--
+--    "It is hard to think of better examples of strong synergy between
+--     theory and practice than the development of programming language
+--     technologies."
+--                                                -- Harald Søndergaard
+--
+-- Well-chosen team name:              pli-dream-team-twentee-nineteen
+-- Well-chosen team members:
+-- * Alan Ung                          alanu
+-- * David Stern                       dibstern
+-- * Dongge Liu                        donggel
+-- * Mariam Shahid                     mariams
+-- * Matthew Farrugia-Roberts          farrugiam
+--
+-- ----------------------------------------------------------------------------
+
+import System.Environment
+
+import Util.ColourParTTY (stripControlChars, rainbowMask, chunkControlChars)
+
+
+-- ----------------------------------------------------------------------------
+-- Output configuration
+-- ----------------------------------------------------------------------------
+
+width = 70      -- maximum output line width (long lines are wrapped)
+point = 42      -- the answer to life, the universe, and everything
+colourSpeed = 3 -- how much rainbow can you handle?
+
+
+-- ----------------------------------------------------------------------------
+-- Program entry-point
+-- ----------------------------------------------------------------------------
+
+main :: IO ()
+main
+  = do
+      speechBubbleTop
+      contents <- getContents
+      let contents' = filter (/='\r') contents
+      mapM speechBubbleLongLine $ lines contents'
+      speechBubbleBot
+
+      args <- getArgs
+      case args of
+        ["-l"] -> rainbow haraldForLightTerminal
+        ["-d"] -> rainbow haraldForDarkTerminal
+        _ -> do
+          rainbow haraldForDarkTerminal
+          putStrLn "WARNING:"
+          putStrLn "Your viewing experience may not be optimal if you are not"
+          putStrLn "using a dark terminal. Please specify -d for dark terminals"
+          putStrLn "or -l for light terminal to silence this warning. Thanks."
+
+
+-- ----------------------------------------------------------------------------
+-- Constructing the speech bubble
+-- ----------------------------------------------------------------------------
+
+speechBubbleTop :: IO ()
+speechBubbleTop
+  = do
+      putChar '/'
+      putStr $ replicate width '`'
+      putChar '\\'
+      putChar '\n'
+
+speechBubbleBot :: IO ()
+speechBubbleBot
+  = do
+      putChar '\\'
+      putStr $ replicate point '_'
+      putStr $ replicate 5  ' '
+      putStr $ replicate (width - 5 - point) '_'
+      putChar '/'
+      putChar '\n'
+
+speechBubbleLongLine :: String -> IO ()
+speechBubbleLongLine line
+  = mapM_ speechBubbleSingleLine $ chunkControlChars (width-1) line
+      
+speechBubbleSingleLine :: String -> IO ()
+speechBubbleSingleLine line
+  = do
+      let len = length $ stripControlChars line
+      putChar '|'
+      putChar ' '
+      putStr line
+      putStr $ replicate (width - 1 - len) ' '
+      putChar '|'
+      putChar '\n'
+
+
+-- ----------------------------------------------------------------------------
+-- Ascii art
+-- ----------------------------------------------------------------------------
+
+-- rainbow
+-- Print an ascii-art list of lines with a 256 colour rainbow
+rainbow :: [String] -> IO ()
+rainbow speakerLines
+  = do
+      -- Get a function that will turn a line's # symbols into a rainbow:
+      let colourer = rainbowMask (== '#') colourSpeed
+      -- Run it over the lines of the speaker's ASCII art:
+      let colouredLines = zipWith colourer [0,colourSpeed..] speakerLines
+      -- Put those lines on the screen!
+      putStr $ unlines colouredLines
+
+-- haraldForLightTerminal, haraldForDarkTerminal
+-- A dedication to our favourite lecturer
+haraldForLightTerminal :: [String]
+haraldForLightTerminal
+  = [ "#################`.------...`##############|   /########################"
+    , "###############`.--...`````.-..-`##########|  /#########################"
+    , "#############`:so-.``       ``./ys-########| /##########################"
+    , "############-yd+..``         ``-:oh/#######|/###########################"
+    , "###########:hh:-.``          ``.-:/o:.##################################"
+    , "##########:sh:..```          ``..-//++.#################################"
+    , "##########oy+--.``            ```-+++sy#################################"
+    , "##########oh/:-..```````   ``--..:oosyh#################################"
+    , "#########`ds/:---.--/++/. `/sso//::+oos#################################"
+    , "##########oo/:---///+//..``o/.....`:oo-#################################"
+    , "##########`:/::.````````.-`++.``  `-o/-#################################"
+    , "###########..::-``    `.--`.so-` `.+s+-#################################"
+    , "###########``//-..```:-/yo+ymNhy/:+sso:#################################"
+    , "###########`-++:-../o:..--+oyhyhhooss+.#################################"
+    , "############./+:-..+:+so+++ohmmh:.//s.##################################"
+    , "##############/+:....-:+//++os+...oo+###################################"
+    , "##############.oo:--.``.-:/+//:.:sho`###################################"
+    , "##############:o/oo//.`````..../hmh`####################################"
+    , "#############:h+/:oyss/-.---:+ymNm+#####################################"
+    , "###########`/oyo://ossydmmmmNMMNmds`####################################"
+    , "#########`-+sssy+--/+yhdNNMMMMNmddMh`###################################"
+    , "#####`.:oyosssyyys-.-/ohdmNNMNmdddMNy.##################################"
+    , "##`:+osyhdsyssyysyys/:/+osyyhdddhmNmdhh+`###############################"
+    ]
+haraldForDarkTerminal :: [String]
+haraldForDarkTerminal
+  = [ "###########################################|   /########################"
+    , "#################mdhhhhhhddd###############|  /#########################"
+    , "################dhhdddmmmmmdhddd###########| /##########################"
+    , "##############h+ohddmNMMMMNNmdhs/+d########|/###########################"
+    , "############h+:shddmNNNNNMNNNNdhyo/y####################################"
+    , "###########y//yhddmNMMMNMMNNNmmhhysoym##################################"
+    , "##########h+/yhhmmmNNMMNNNNNNmdddhsyosd#################################"
+    , "##########o/ohhhmmNNNMNNNNNNNNmmdhoss++#################################"
+    , "##########o/syhhdmmmmmmNNNNmmhdddyooo+/#################################"
+    , "##########:+syhyhhhhysoydNms++osyyysooo#################################"
+    , "##########oosyhhhyyysyyddmmoyddmmdmyooh#################################"
+    , "##########mysyyhmmmmmmmddhmssdmNNNmhosd#################################"
+    , "###########dhyyhmmNNNNmhhhmd+ohmNmds+sd#################################"
+    , "###########mmysyddmNmyhs/oo+../+yys++oh#################################"
+    , "###########mhssyhhhyoydhhhs+/////oo++sm#################################"
+    , "############mysyhhdsyo+ossoo/-.:ydyy+m##################################"
+    , "##############ysyddddhyssysso+sdddoos###################################"
+    , "##############mooyhhdmmdhyysssydy+/s####################################"
+    , "##############yosoossdmmmmmddddy/-/#####################################"
+    , "#############h:ssyo++oyhdhhhhs/-.-s#####################################"
+    , "############yo/oysso++/:-...```.-:+#####################################"
+    , "#########mds+++/shhyo+/:-.`  `.-::`/####################################"
+    , "#####mdho+o++++++ohdhso/:--...-:/:`./m##################################"
+    , "####so++::+++++++//+sysoo++//////..-://s################################"
+    ]
--- src/Util/ColourParTTY.hs
+++ src/Util/ColourParTTY.hs
@@ -1,21 +1,34 @@
 module Util.ColourParTTY where
 
-import Data.List (cycle)
+-- ----------------------------------------------------------------------------
+--    COMP90045 Programming Language Implementation, Assignment Stage 3
+--
+--                You can't spell party without TTY- oh, wait...
+--
+-- Well-chosen team name:              pli-dream-team-twentee-nineteen
+-- Well-chosen team members:
+-- * Alan Ung                          alanu
+-- * David Stern                       dibstern
+-- * Dongge Liu                        donggel
+-- * Mariam Shahid                     mariams
+-- * Matthew Farrugia-Roberts          farrugiam
+--
+-- ----------------------------------------------------------------------------
+
+
+-- ----------------------------------------------------------------------------
+-- Making and using ANSI colour codes
+-- ----------------------------------------------------------------------------
 
 type Colour
   = String
 type Colourer
   = Colour -> String
 
-
+-- Basic colours
 esc :: Int -> Colour
 esc code
   = "\ESC[" ++ show code ++ "m"
--- esc5 code
---   = "\ESC[5;" ++ show code ++ "m"
-
-reset :: Colour
-reset   = esc 0
 
 setBlk1, setRed1, setGrn1, setYel1, setBlu1, setMgn1, setCyn1, setWht1 :: Colour
 setBlk1 = esc 30
@@ -37,50 +50,137 @@ setMgn2 = esc 95
 setCyn2 = esc 96
 setWht2 = esc 97
 
-with :: Colour -> Colourer
-with colour string
+-- Set the terminal back to default colours
+reset :: Colour
+reset   = esc 0
+
+
+-- For truecolour terminals
+escTrue :: (Int, Int, Int) -> Colour
+escTrue (r, g, b)
+  = "\ESC[38;2;" ++ show r ++ ";" ++ show g ++ ";" ++ show b ++ "m"
+
+-- For 256-colour terminals
+-- (n is in a 6x6x6 grid of colours from 16 to 231, or before 16 for basic
+-- colours, or after 231 for greyscale colours)
+esc256 :: Int -> Colour
+esc256 n
+  = "\ESC[38;5;" ++ show n ++ "m"
+
+-- Cycle through truecolours
+colourStepTrue :: Int -> (Int, Int, Int)
+colourStepTrue i
+  = (r, g, b)
+  where
+    rad = (fromIntegral i) / 180 * pi
+    r = round $ 128 + 127 * (sin rad)
+    g = round $ 128 + 127 * (sin $ rad - 2 * pi / 3)
+    b = round $ 128 + 127 * (sin $ rad - 4 * pi / 3)
+
+-- Cycle through more basic 8-bit colours instead
+-- (Not all terminals support truecolour!)
+colourStep256 :: Int -> Int
+colourStep256 i
+  = 16 + 36*r6 + 6*g6 + b6
+  where
+    (r256, g256, b256) = colourStepTrue i
+    r6 = floor $ (fromIntegral r256 / 256) * 6
+    g6 = floor $ (fromIntegral g256 / 256) * 6
+    b6 = floor $ (fromIntegral b256 / 256) * 6
+
+
+-- ----------------------------------------------------------------------------
+-- Applying colouring to strings
+-- ----------------------------------------------------------------------------
+
+-- Wrap a string in a colour
+withColour :: Colour -> Colourer
+withColour colour string
   = colour ++ string ++ reset
 
--- dark Colours
+-- dark Colours as functions
 blk1, red1, grn1, yel1, blu1, mgn1, cyn1, wht1 :: Colourer
-blk1 = with setBlk1
-red1 = with setRed1
-grn1 = with setGrn1
-yel1 = with setYel1
-blu1 = with setBlu1
-mgn1 = with setMgn1
-cyn1 = with setCyn1
-wht1 = with setWht1
+blk1 = withColour setBlk1
+red1 = withColour setRed1
+grn1 = withColour setGrn1
+yel1 = withColour setYel1
+blu1 = withColour setBlu1
+mgn1 = withColour setMgn1
+cyn1 = withColour setCyn1
+wht1 = withColour setWht1
 
--- bright Colours
+-- bright Colours as functions
 blk2, red2, grn2, yel2, blu2, mgn2, cyn2, wht2 :: Colourer
-blk2 = with setBlk2
-red2 = with setRed2
-grn2 = with setGrn2
-yel2 = with setYel2
-blu2 = with setBlu2
-mgn2 = with setMgn2
-cyn2 = with setCyn2
-wht2 = with setWht2
+blk2 = withColour setBlk2
+red2 = withColour setRed2
+grn2 = withColour setGrn2
+yel2 = withColour setYel2
+blu2 = withColour setBlu2
+mgn2 = withColour setMgn2
+cyn2 = withColour setCyn2
+wht2 = withColour setWht2
 
 
-colours1 :: [Colour]
-colours1
-  = [setRed1, setYel1, setGrn1, setCyn1, setBlu1, setMgn1]
+-- Cycle those characters in a string matching a certain predicate
+-- through colours, and 
+rainbowMask :: (Char -> Bool) -> Int -> Int -> (String -> String)
+rainbowMask mask speed offset string
+  = concat $ zipWith wrapMaskChar colours string
+  where
+    colours
+      = drop offset $ map (esc256 . colourStep256) [0,speed..]
+    wrapMaskChar colour char
+      | mask char = colour ++ char:"" ++ reset
+      | otherwise = char:""
 
-colours2 :: [Colour]
-colours2
-  = [setRed2, setYel2, setGrn2, setCyn2, setBlu2, setMgn2]
 
-rainbow :: [Colour] -> String -> String
-rainbow cols string
-  = interleave (cycle cols) string
-    where
-      interleave _ [] = reset
-      interleave (c:cs) (s:ss) = c ++ s : interleave cs ss
+-- Default functions to cycle a string through various colours
+rainbow :: Int -> Int -> String -> String
+rainbow
+  = rainbowMask (const True)
 
-rainbow1
-  = rainbow colours1
 
-rainbow2
-  = rainbow (drop 1 colours2) -- offset is nice?
+-- ----------------------------------------------------------------------------
+-- Manipulating strings containing ANSI control codes.
+-- ----------------------------------------------------------------------------
+
+-- stripControlChars
+-- Attempt to remove ANSI control codes from a string. Assumes only a single 
+-- layer of control characters... not very robust!
+stripControlChars :: String -> String
+stripControlChars []
+  = []
+stripControlChars ('\ESC':restOfString)
+  = stripControlChars $ tail $ dropWhile (/= 'm') $ restOfString
+stripControlChars (c:cs)
+  = c : stripControlChars cs
+
+-- chunkControlChars
+-- Split a string containing control sequences into chunks of a given length,
+-- NOT counting the control sequences, but leaving them in the string.
+chunkControlChars :: Int -> String -> [String]
+chunkControlChars _ []
+  = []
+chunkControlChars n string
+  = line1 : chunkControlChars n restOfString
+  where 
+    (line1, restOfString) = initDropControlChars n string
+
+-- initDropControlChars
+-- Split away a given number of characters from the start of a string, NOT 
+-- counting control sequences, but leaving them in the strings.
+initDropControlChars :: Int -> String -> (String, String)
+initDropControlChars n ('\ESC':restOfString)
+  = (escapeSequence ++ start, rest)
+  where
+    escapeSequence = '\ESC' : (takeWhile (/= 'm') restOfString) ++ "m"
+    afterEscapeSeq = tail $ dropWhile (/= 'm') restOfString
+    (start, rest)  = initDropControlChars n afterEscapeSeq
+initDropControlChars 0 s
+  = ("", s)
+initDropControlChars n []
+  = ("", "")
+initDropControlChars n (c:cs)
+  = (c : start, rest)
+  where
+     (start, rest) = initDropControlChars (n-1) cs
